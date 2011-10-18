6fe9930d6a4aa216d1ab7f6ec6a71b0d717048af
Tim Henderson <tim.tadh@gmail.com>
2011-10-18 03:14:05 -0400

    non-term count added
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- artifacts/tables.py
+++ artifacts/tables.py
@@ -36,3 +36,30 @@ def symbol_count(conf):
     f.write('\n'*2)
     f.close()
 
+@registration.register('non_term_count', 'table')
+def non_term_count(conf):
+    fname = os.path.join(conf['outdir'], 'non_term_count.csv')
+    symbols = dict()
+    trees = conf['trees']
+   
+    def walk(node):
+        stack = list()
+        stack.append(node)
+        while stack:
+            node = stack.pop()
+            if node.children: symbols[node.label] = symbols.get(node.label, 0) + 1
+            for child in node.children:
+                stack.append(child)
+    
+    for tree in trees:
+        walk(tree)
+    
+    s = '\n'.join(
+        ', '.join((name, str(count))) for name, count in symbols.iteritems()
+    )
+
+    f = open(fname, 'w')
+    f.write(s)
+    f.write('\n'*2)
+    f.close()
+
